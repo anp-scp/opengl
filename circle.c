dfb66703ff676d90fb5871a1bb49abe067eaaca3
scpkiller <anupammrg@gmail.com>
2018-05-05 02:40:07 +0530

    Modified midpoint circle drawing algo

--- circle.c
+++ circle.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-#include<math.h>
 #include<GL/glut.h>
+#include<math.h>
+#include<stdlib.h>
 
-int x,y;
-void Init()
+// function to initialize
+void myInit ()
 {
 	// making background color black as first 
 	// 3 arguments all are 0.0
@@ -18,38 +19,14 @@ void Init()
 	glLoadIdentity();
 	
 	// setting window dimension in X- and Y- direction
-	gluOrtho2D(-800, 800, -800, 800);
+	gluOrtho2D(-780, 780, -420, 420);
 }
 
-void ddaLineAlgo(int x1, int Y1, int x2, int Y2, float r, float g, float b)
+void circle(float adj)
 {
-	int i;
-	float x,y,dx,dy,step,xinc,yinc;
-	glColor3f(r,g,b);
-	dx = (x2-x1);
-	dy = (Y2-Y1);
-	if(abs(dx) > abs(dy))
-		step = abs(dx);
-	else 
-		step = abs(dy);
-	xinc = dx/step;
-	yinc = dy/step;
-	i = 1; x = x1; y = Y1;
-	while(i<=step)
-	{
-		glVertex2d(round(x),round(y));
-		i++;
-		x += xinc;
-		y += yinc;
-	}
-	glVertex2d(round(x),round(y));
-}
-
-void circle()
-{
-	float p;
+	float x, y, p;
+	x = 0; y = 100;
 	p = (5/4)-y;
-	//glColor3f(1.0,0.0,0.0);
 	while(x<=y)
 	{
 		x++;
@@ -60,45 +37,47 @@ void circle()
 			y--;
 			p = p+(2*x)-(2*y) +1;
 		}
-		printf("\n%d %d",x,y);
-		glVertex2d(round(x),round(y));
-		glVertex2d(y,x);
-		glVertex2d(x,-y);
-		glVertex2d(y,-x);
-		glVertex2d(-x,-y);
-		glVertex2d(-y,-x);
-		glVertex2d(-x,y);
-		glVertex2d(-y,x);
+		glVertex2d(x,y+(adj));
+		glVertex2d(y,x+(adj));
+		glVertex2d(x,-y+(adj));
+		glVertex2d(y,-x+(adj));
+		glVertex2d(-x,-y+(adj));
+		glVertex2d(-y,-x+(adj));
+		glVertex2d(-x,y+(adj));
+		glVertex2d(-y,x+(adj));
 	}
 }
 
-void displayCB(void)
+void display (void) 
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POINTS);
-	ddaLineAlgo(-800,0,800,0,1.0,0.0,0.0);
-	circle();
+	circle(0);
 	glEnd();
-	glFlush();	
+	glFlush();
 }
 
-void keyCB(unsigned char key, int x,int y)
+void keyCB(unsigned char k, int x, int y)
 {
-	if(key == 'q')
+	if(k == 'q')
 		exit(0);
 }
 
-void main(int argc, char **argv)
+int main (int argc, char** argv)
 {
-	printf("\nEnter x, r:");
-	scanf("%d%d",&x,&y);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(0,0);
-	glutInitWindowSize(600,600);
-	glutCreateWindow("DDA Line");
-	Init();
-	glutDisplayFunc(displayCB);
+	
+	// giving window size in X- and Y- direction
+	glutInitWindowSize(1366, 768);
+	glutInitWindowPosition(0, 0);
+	
+	// Giving name to window
+	glutCreateWindow("Circle Drawing");
+	myInit();
+	
+	glutDisplayFunc(display);
 	glutKeyboardFunc(keyCB);
 	glutMainLoop();
 }
+
